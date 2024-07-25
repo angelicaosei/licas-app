d38135879169bdfcfa414f52f19f46746af1eb73
angelicaosei <licaosei7@gmail.com>
2024-07-25 12:48:01 +0000

    added the API integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function currentWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchcity(city) {
+  let apikey = "b2a5adcct04b33178913oc335f405433";
+  let apiurl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  axios.get(apiurl).then(currentWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
-
   let searchInput = document.querySelector("#search-inputForm");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchcity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
